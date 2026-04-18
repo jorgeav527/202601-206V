142ed8507cda0a9b2afa3e34152ba6ad26b5032b
jorgeav527 <jorgeav527@gmail.com>
2026-04-17 20:58:54 -0500

    feat: post and get

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,11 +17,19 @@ def r_w():
     return content
 
 
-@app.route("/count")
+@app.route("/count", methods=["GET", "POST"])
 def count():
-    with open("count.txt", mode="r") as file:
-        count = int(file.read())
-    count += 1
-    with open("count.txt", mode="w") as file:
-        file.write(str(count))
-    return f'Numero de visitas: {count}'
+    if request.method == "GET":
+        forma = """
+        <form action="/count" method="post">
+            <button type="submit">Incrementar</button>
+        </form>
+        """
+        return forma
+    if request.method == "POST":
+        with open("count.txt", mode="r") as file:
+            count = int(file.read())
+        count += 1
+        with open("count.txt", mode="w") as file:
+            file.write(str(count))
+        return f'Numero de visitas: {count}'
