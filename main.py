e142a3eff16007acd2f20171ad521ae14423122f
richardgalvez2608 <richardgalvez2608@gmail.com>
2026-04-19 02:20:43 -0500

    feat: adding route hello and ch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from 202601-206v!")
+from flask import Flask
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/ch")
+def check_held():
+    return "OK"
