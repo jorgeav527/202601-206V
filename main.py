be4d695f1f2b10c4c6750920b1ba241ae82b27c3
richardgalvez2608 <richardgalvez2608@gmail.com>
2026-04-19 03:18:31 -0500

    feat: route count and readme

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hola, Richard!</p>"
 
 @app.route("/ch")
 def check_held():
     return "OK"
+
+@app.route("/readme")
+def r_w():
+    with open("README.md") as file:
+        content = file.read()
+    return content 
+
+
+@app.route("/count")
+def count():
+    with open("count.txt", mode="r") as file:
+        count = int(file.read())
+    count += 1
+    with open("count.txt", mode="w") as file:
+        file.write(str(count))
+    return f'Numero de visitas: {count}' 
