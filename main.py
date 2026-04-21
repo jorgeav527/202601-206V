7cdafa4cecc3e93a136dd50a5505ae3928a99c09
jorgeav527 <jorgeav527@gmail.com>
2026-04-20 20:32:27 -0500

    feat: list posts

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('basedatos.db')
-    # conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+@app.route('/post', methods=['GET'])
+def get_all_post():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('post/list.html', posts=posts)
 
 @app.route("/checkheald")
 def hello_world():
