6e974a74c9e33f8951cacec739f58464ab6c2edf
jorge alarcon <jorgeav527@gmail.com>
2026-04-22 17:46:56 -0500

    feat: basic routes are finished

---
+++ init_db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+connection = sqlite3.connect('basedatos.db')
+
+with open('schema.sql', 'w') as f:
+    f.write("""
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+);
+""")
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
